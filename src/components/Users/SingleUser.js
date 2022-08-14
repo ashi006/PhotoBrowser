f6a5b2c6756ed1db6414e541e808db625bf65675
asma jamil <asma.jamil@inara.pk>
2022-08-14 05:06:48 +0500

    Final changes

--- src/components/Users/SingleUser.js
+++ src/components/Users/SingleUser.js
@@ -6,7 +6,7 @@ import UserIcon from '../../images/user-icon-2.jpg';
 import Albums from '../Albums/Albums';
 import { useParams } from 'react-router-dom';
 import { getUserDetails } from '../../api';
-import { FaUser, FaEnvelope, FaPhone, FaGlobe, FaMapMarker } from 'react-icons/fa';
+import { FaUser, FaEnvelope, FaPhone, FaGlobe, FaMapMarker, FaBuilding } from 'react-icons/fa';
 
 const SingleUser = () => {
     const { id } = useParams();
@@ -25,25 +25,23 @@ const UserDetail = ({resource}) => {
     return (
         <>
             <Row style={{backgroundColor: "#fff"}}>
-            <Col lg={2}><img src={UserIcon} alt="" style={{width: "100%"}} /></Col>
-            <Col lg={10}>
+            <Col lg={2} sm={6} style={{textAlign:"center"}}>
+                <img className="single-user-pic" src={UserIcon} alt="" />
+            </Col>
+            <Col lg={10} sm={6}>
                 <Row>
-                    <Col lg={3}>
+                    <Col lg={6}>
                         <ul className="single-user-detail">
                             <li><FaUser /> &nbsp;{userDetails.name} ({userDetails.username})</li>
                             <li><FaEnvelope /> &nbsp;{userDetails.email}</li>
-                        </ul>
-                    </Col>
-                    <Col lg={3}>
-                        <ul className="single-user-detail">
                             <li><FaPhone /> &nbsp;{userDetails.phone}</li>
-                            <li><FaGlobe /> &nbsp;<a href={userDetails.website} target="_blank" rel="noreferrer">{userDetails.website}</a></li>
                         </ul>
                     </Col>
                     <Col lg={6}>
                         <ul className="single-user-detail">
+                            <li><FaGlobe /> &nbsp;<a href={userDetails.website} target="_blank" rel="noreferrer">{userDetails.website}</a></li>
+                            <li><FaBuilding/> &nbsp;{userDetails.company.name}</li>
                             <li><FaMapMarker/> &nbsp;{userDetails.address.suite}, {userDetails.address.street}, {userDetails.address.city}</li>
-                            <li><FaMapMarker/> &nbsp;{userDetails.company.name}</li>
                         </ul>
                     </Col>
                 </Row>
